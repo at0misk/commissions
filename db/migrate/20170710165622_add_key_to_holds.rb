6ed9317d11cb2525eb846b6ae38e1a5d88f2ce19
at0misk <ktp925@gmail.com>
2017-07-10 11:31:35 -0700

    holds

--- db/migrate/20170710165622_add_key_to_holds.rb
+++ db/migrate/20170710165622_add_key_to_holds.rb
@@ -1,6 +1,6 @@
 class AddKeyToHolds < ActiveRecord::Migration[5.0]
   def change
-	# add_reference :holds, :evo, references: :users, index: true
-	# add_foreign_key :holds, :users, column: :evo_id
+	add_reference :holds, :evo, references: :users, index: true
+	add_foreign_key :holds, :users, column: :evo_id
   end
 end
