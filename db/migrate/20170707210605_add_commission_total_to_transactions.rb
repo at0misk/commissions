d8e0fa78a0c111d9737f7cf48d847101acaeaf42
at0misk <ktp925@gmail.com>
2017-07-10 09:08:02 -0700

    db

--- db/migrate/20170707210605_add_commission_total_to_transactions.rb
+++
@@ -1,5 +0,0 @@
-class AddCommissionTotalToTransactions < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :transactions, :commission_total, :integer
-  end
-end
--- db/migrate/20170710151219_drop_total_from_commission.rb
+++
@@ -1,5 +0,0 @@
-class DropTotalFromCommission < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :transactions, :commission_total
-  end
-end
