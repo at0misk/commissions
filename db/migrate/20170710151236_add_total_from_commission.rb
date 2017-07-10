326e41710f8dfa09559d1b0fd1ed4d016881f5f4
at0misk <ktp925@gmail.com>
2017-07-10 09:15:36 -0700

    db

--- db/migrate/20170710151236_add_total_from_commission.rb
+++ db/migrate/20170710151236_add_total_from_commission.rb
@@ -1,5 +1,5 @@
 class AddTotalFromCommission < ActiveRecord::Migration[5.0]
   def change
-  	add_column :transactions, :commission_total, :decimal, :precision => 8, :scale => 4
+  	# add_column :transactions, :commission_total, :decimal, :precision => 8, :scale => 4
   end
 end
