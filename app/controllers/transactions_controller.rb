15747383ebcf96f580421984f6de0a59f97b7091
at0misk <ktp925@gmail.com>
2017-09-08 15:12:19 -0700

    waT

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -57,7 +57,7 @@ class TransactionsController < ApplicationController
 			  		val.sponsor_c2go = @upline.c2go
 			  		# val.sponsor_name = @upline.first + " " + @upline.last
 			  	end
-		  		val.agent_name = @u.first + " " + @u.last
+		  		# val.agent_name = @u.first + " " + @u.last
 		  		if @u.processed
 					val.processed = true
 				else
